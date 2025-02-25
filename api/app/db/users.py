04e1d4164d9ea156277f7c9ddec05bee298cf8ce
Olivier Ribiere <o_ribiere@cartong.org>
2025-02-25 16:05:33 +0100

    feat(users): add roles

--- api/app/db/users.py
+++ api/app/db/users.py
@@ -1,6 +1,8 @@
 from pydantic import EmailStr
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import ARRAY, Column, SQLModel, Field
+from typing import List, Optional
+from app.models.user_roles import UserRole
+from sqlalchemy import Enum as SAEnum
 
 
 class User(SQLModel, table=True):
@@ -8,3 +10,4 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, index=True)
     email: EmailStr = Field(unique=True, index=True)
     hashed_password: str
+    roles: List[UserRole] = Field(sa_column=Column(ARRAY(SAEnum(UserRole))))
--- api/app/models/users.py
+++ api/app/models/users.py
@@ -1,9 +1,13 @@
+from typing import List
 from pydantic import BaseModel, EmailStr
+
+from app.models.user_roles import UserRole
 
 
 class UserBase(BaseModel):
     email: EmailStr
     username: str
+    roles: List[UserRole] = [UserRole.USER]
 
 
 class UserLogin(BaseModel):
